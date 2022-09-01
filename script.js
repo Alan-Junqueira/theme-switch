eb4a276f4635a2b99624ede03b614ce96ad2646e
Alan Junqueira <contato.alanjunqueira@gmail.com>
2022-08-31 21:02:48 -0300

    adjust width logic

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const switchTheme = () => {
 
   arrayImageAndSwitch.forEach((item) => {
     item.addEventListener('click', (event) => {
-      if (screen.availWidth === 1440) {
-        if (event.clientX < 680) {
+      if (screen.availWidth >= 1331 && screen.availWidth <= 1440) {
+        if (event.clientX < 700) {
           circleSwitch.classList.add('darkThemeSwitcher');
           circleSwitch.classList.remove('lightThemeSwitcher');
           body.classList.add('darkThemeBg');
@@ -19,48 +19,44 @@ const switchTheme = () => {
           body.classList.remove('darkThemeBg');
           body.classList.add('lightThemeBg');
         }
-      } else if (screen.availWidth === 1024) {
+      } else if (screen.availWidth >= 1171 && screen.availWidth <= 1330) {
+        if (event.clientX < 625) {
+          circleSwitch.classList.add('darkThemeSwitcher');
+          circleSwitch.classList.remove('lightThemeSwitcher');
+          body.classList.add('darkThemeBg');
+          body.classList.remove('lightThemeBg');
+        } else {
+          circleSwitch.classList.add('lightThemeSwitcher');
+          circleSwitch.classList.remove('darkThemeSwitcher');
+          body.classList.remove('darkThemeBg');
+          body.classList.add('lightThemeBg');
+        }
+      } else if (screen.availWidth >= 1025 && screen.availWidth <= 1170) {
+        if (event.clientX < 545) {
+          circleSwitch.classList.add('darkThemeSwitcher');
+          circleSwitch.classList.remove('lightThemeSwitcher');
+          body.classList.add('darkThemeBg');
+          body.classList.remove('lightThemeBg');
+        } else {
+          circleSwitch.classList.add('lightThemeSwitcher');
+          circleSwitch.classList.remove('darkThemeSwitcher');
+          body.classList.remove('darkThemeBg');
+          body.classList.add('lightThemeBg');
+        }
+      } else if (screen.availWidth >= 931 && screen.availHeight <= 1024) {
         if (event.clientX < 475) {
           circleSwitch.classList.add('darkThemeSwitcher');
           circleSwitch.classList.remove('lightThemeSwitcher');
+          body.classList.add('darkThemeBg');
+          body.classList.remove('lightThemeBg');
         } else {
           circleSwitch.classList.add('lightThemeSwitcher');
           circleSwitch.classList.remove('darkThemeSwitcher');
+          body.classList.remove('darkThemeBg');
+          body.classList.add('lightThemeBg');
         }
-      } else if (screen.availWidth === 768) {
-        if (event.clientX < 350) {
-          circleSwitch.classList.add('darkThemeSwitcher');
-          circleSwitch.classList.remove('lightThemeSwitcher');
-        } else {
-          circleSwitch.classList.add('lightThemeSwitcher');
-          circleSwitch.classList.remove('darkThemeSwitcher');
-        }
-      } else if (screen.availWidth <= 425) {
-        if (event.clientX < 170) {
-          circleSwitch.classList.add('darkThemeSwitcher');
-          circleSwitch.classList.remove('lightThemeSwitcher');
-        } else {
-          circleSwitch.classList.add('lightThemeSwitcher');
-          circleSwitch.classList.remove('darkThemeSwitcher');
-        }
-      } else if (screen.availWidth <= 375) {
-        if (event.clientX < 165) {
-          circleSwitch.classList.add('darkThemeSwitcherSmallScreen');
-          circleSwitch.classList.remove('lightThemeSwitcherSmallScreen');
-        } else {
-          circleSwitch.classList.add('lightThemeSwitcherSmallScreen');
-          circleSwitch.classList.remove('darkThemeSwitcherSmallScreen');
-        }
-      } else if (screen.availWidth <= 320) {
-        if (event.clientX < 155) {
-          circleSwitch.classList.add('darkThemeSwitcherVerySmallScreen');
-          circleSwitch.classList.remove('lightThemeSwitcherVerySmallScreen');
-        } else {
-          circleSwitch.classList.add('lightThemeSwitcherVerySmallScreen');
-          circleSwitch.classList.remove('darkThemeSwitcherVerySmallScreen');
-        }
-      } else if (screen.availWidth > 1440) {
-        if (event.clientX < 680) {
+      } else if (screen.availWidth >= 769 && screen.availHeight <= 930) {
+        if (event.clientX < 425) {
           circleSwitch.classList.add('darkThemeSwitcher');
           circleSwitch.classList.remove('lightThemeSwitcher');
           body.classList.add('darkThemeBg');
@@ -71,8 +67,8 @@ const switchTheme = () => {
           body.classList.remove('darkThemeBg');
           body.classList.add('lightThemeBg');
         }
-      } else if (screen.availWidth > 1024) {
-        if (event.clientX < 650) {
+      } else if (screen.availWidth >= 581 && screen.availHeight <= 768) {
+        if (event.clientX < 340) {
           circleSwitch.classList.add('darkThemeSwitcher');
           circleSwitch.classList.remove('lightThemeSwitcher');
           body.classList.add('darkThemeBg');
@@ -80,6 +76,42 @@ const switchTheme = () => {
         } else {
           circleSwitch.classList.add('lightThemeSwitcher');
           circleSwitch.classList.remove('darkThemeSwitcher');
+          body.classList.remove('darkThemeBg');
+          body.classList.add('lightThemeBg');
+        }
+      } else if (screen.availWidth >= 425 || screen.availHeight <= 580) {
+        if (event.clientX < 245) {
+          circleSwitch.classList.add('darkThemeSwitcher');
+          circleSwitch.classList.remove('lightThemeSwitcher');
+          body.classList.add('darkThemeBg');
+          body.classList.remove('lightThemeBg');
+        } else {
+          circleSwitch.classList.add('lightThemeSwitcher');
+          circleSwitch.classList.remove('darkThemeSwitcher');
+          body.classList.remove('darkThemeBg');
+          body.classList.add('lightThemeBg');
+        }
+      } else if (screen.availWidth === 375) {
+        if (event.clientX < 165) {
+          circleSwitch.classList.add('darkThemeSwitcherSmallScreen');
+          circleSwitch.classList.remove('lightThemeSwitcherSmallScreen');
+          body.classList.add('darkThemeBg');
+          body.classList.remove('lightThemeBg');
+        } else {
+          circleSwitch.classList.add('lightThemeSwitcherSmallScreen');
+          circleSwitch.classList.remove('darkThemeSwitcherSmallScreen');
+          body.classList.remove('darkThemeBg');
+          body.classList.add('lightThemeBg');
+        }
+      } else if (screen.availWidth === 320) {
+        if (event.clientX < 155) {
+          circleSwitch.classList.add('darkThemeSwitcherVerySmallScreen');
+          circleSwitch.classList.remove('lightThemeSwitcherVerySmallScreen');
+          body.classList.add('darkThemeBg');
+          body.classList.remove('lightThemeBg');
+        } else {
+          circleSwitch.classList.add('lightThemeSwitcherVerySmallScreen');
+          circleSwitch.classList.remove('darkThemeSwitcherVerySmallScreen');
           body.classList.remove('darkThemeBg');
           body.classList.add('lightThemeBg');
         }
